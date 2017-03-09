d2607e9258d013a09d45e487488074c60e7664dc
KumagaiNaoki <j.a.r.n0405@gmail.com>
2017-03-09 16:39:19 +0900

    ログアウト実装

--- APIKit+Himotoki+InstagramAPI/ViewController.swift
+++ APIKit+Himotoki+InstagramAPI/ViewController.swift
@@ -12,12 +12,23 @@ import APIKit
 
 class ViewController: UIViewController {
     
+    var CLIENTURL = ""
+    var ACCESSTOKENURL = ""
+    
     let webView = UIWebView()
+    let ud = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let ud = UserDefaults.standard
+        let env = ProcessInfo.processInfo.environment
+        if let value = env["CLIENTURL"] {
+            CLIENTURL = value
+        }
+        if let value = env["ACCESSTOKENURL"] {
+            ACCESSTOKENURL = value
+        }
+        
         if (ud.object(forKey: "accessToken") != nil) {
             let accessToken = ud.string(forKey: "accessToken")
             requestAPI(accessToken: accessToken!)
@@ -30,12 +41,14 @@ class ViewController: UIViewController {
         webView.delegate = self
         webView.frame = view.frame
         view.addSubview(webView)
-        let url = URL(string: "***********************************秘密********************************************")
+        let url = URL(string: CLIENTURL)
         let request = URLRequest(url: url!)
         webView.loadRequest(request)
     }
     
     func requestAPI(accessToken: String) {
+        logoutButton()
+        
         let request = GetInstagramRequest(accessToken: accessToken)
         Session.send(request) { result in
             switch result {
@@ -51,14 +64,32 @@ class ViewController: UIViewController {
                     let label = imageModel.dateFormatter(dateString: instagramData.createdDate[count])
                     label.frame = CGRect(x: 0, y: 0, width: imageView.frame.width, height: 15)
                     imageView.addSubview(label)
-                    
                     count += 1
                 }
             case .failure(let error):
                 print("error\(error)")
             }
         }
-
+    }
+    func logoutButton() {
+        let button = UIButton()
+        button.frame = CGRect(x: 300, y: 300, width: 100, height: 15)
+        button.backgroundColor = UIColor.black
+        view.addSubview(button)
+        button.addTarget(self, action: #selector(tapButton(sender:)), for: .touchUpInside)
+    }
+    func addLogoutProcess() {
+    }
+    func tapButton(sender: UIButton) {
+        let cookieJar = HTTPCookieStorage.shared
+        for cookie in cookieJar.cookies! as [HTTPCookie] {
+            print(cookie)
+            if cookie.domain == "www.instagram.com" || cookie.domain == "api.instagram.com" {
+                cookieJar.deleteCookie(cookie)
+                ud.removeObject(forKey: "accessToken")
+            }
+        }
+        displayWebVeiw()
     }
 }
 
@@ -66,10 +97,9 @@ extension ViewController: UIWebViewDelegate {
     func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
         
         if navigationType.rawValue == 1 {
-            let urlArray = request.url?.absoluteString.components(separatedBy: "********************秘密***********************")
+            let urlArray = request.url?.absoluteString.components(separatedBy: ACCESSTOKENURL)
             if urlArray!.count > 1 {
                 let access_token = urlArray?.last
-                print(access_token)//codeだけ綺麗に取れた
                 saveUserDefault(accessToken: access_token!)
                 self.requestAPI(accessToken: access_token!)
             }
@@ -77,7 +107,6 @@ extension ViewController: UIWebViewDelegate {
         return true
     }
     func saveUserDefault(accessToken: String) {
-        let ud = UserDefaults.standard
         ud.set(accessToken, forKey: "accessToken")
         ud.synchronize()
     }
